093a58faffe9da8077bf107c18367854e514fccb
Vid Čufar <vid@cufar.dev>
2024-10-19 19:03:03 +0200

    chore: add shadcn config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/upn-to-epc-qr/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '/src'),
+    },
+  },
 });
